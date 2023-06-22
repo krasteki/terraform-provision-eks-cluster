ce2f79c36db5c938209da2b46e6dd7925e366c43
krasteki <51786552+krasteki@users.noreply.github.com>
2023-06-22 16:09:42 +0300

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.20.0"
+      version = "4.20.0"
     }
 
     random = {
@@ -22,9 +22,9 @@ terraform {
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.19.1"
+      version = "2.19.0"
     }
   }
 
-  required_version = ">= 0.14"
+  required_version = ">=0.14"
 }
